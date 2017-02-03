e36865272d16520ae44dc6502b7f5d1751f23223
babonev <mytablet.profile@gmail.com>
2017-02-03 13:14:37 +0200

    Initial upload.

---
+++ version.h
@@ -0,0 +1,30 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+	//Date Version Types
+	#define DATE "01"
+	#define MONTH "02"
+	#define YEAR "2017"
+	#define UBUNTU_VERSION_STYLE "17.02"
+	
+	//Software Status
+	#define STATUS "Alpha"
+	#define STATUS_SHORT "a"
+	
+	//Standard Version Type
+	#define MAJOR 1
+	#define MINOR 3
+	#define BUILD 319
+	#define REVISION 1764
+	
+	//Miscellaneous Version Types
+	#define BUILDS_COUNT 557
+	#define RC_FILEVERSION 1,3,319,1764
+	#define RC_FILEVERSION_STRING "1, 3, 319, 1764\0"
+	#define FULLVERSION_STRING "1.3.319.1764"
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	#define BUILD_HISTORY 19
+	
+
+#endif //VERSION_H
